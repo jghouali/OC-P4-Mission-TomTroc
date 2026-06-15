ea11459c26e34a7cfe63b5f29515dc63f581622b
green <jghouali@gmail.com>
2026-06-15 09:45:19 +0400

    Issue #49 : Add Validator type Enum

---
+++ src/Enum/ValidatorEnum.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Green\TomTroc\Enum;
+
+enum ValidatorEnum: string
+{
+    case alphanumeric_50 = '50alphanumeric_-';
+    case alphanumeric_150 = '150alphanumeric_-';
+    case bcryptHash = 'bcryptHash';
+    case uploadFile = 'uploadFile';
+    case email = 'email';
+    case humanDate = 'humanDate';
+    case intCounter = 'intCounter';
+}
